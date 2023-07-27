d9b61f34fba88c69e3ae3a85dc108c2d5bcf2c3c
alpezel <allistairevillaruel@gmail.com>
2023-07-28 00:57:54 +0800

    update to record video

--- Class_Tkinter_Covid_Contact_Tracing_App.py
+++ Class_Tkinter_Covid_Contact_Tracing_App.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from Class_Tkinter_Search_Entries import Search_Entry
+
 class CovidContactTracingApp:
     def __init__(self,root):
         self.root = root
