937610852dedcbb3b90b8578fd51750d666aaae3
alpezel <allistairevillaruel@gmail.com>
2023-07-27 16:54:39 +0800

    add a search button, search entries method, and display entries method and then test run

--- Search_Entries.py
+++ Search_Entries.py
@@ -5,9 +5,43 @@ class Search_Entry:
     def __init__(self,root):
         self.root = root
         
-        search_frame = tk.Frame(root)
-        search_frame.pack()
-        self.search_label = tk.Label(search_frame, text="Search:")
+        frame = tk.Frame(root)
+        frame.pack()
+
+        self.search_label = tk.Label(frame, text="Search:")
         self.search_label.pack()
-        self.search_entry = tk.Entry(search_frame)
+        self.search_entry = tk.Entry(frame)
         self.search_entry.pack()
+
+        self.search_button = tk.Button(frame, text="Search", command=self.search_entries)
+        self.search_button.pack()
+
+        self.exit_button = tk.Button(frame, text="Exit", command=root.quit)
+        self.exit_button.pack()
+
+    def search_entries(self):
+        search_term = self.search_entry.get().strip().lower()
+        if search_term:
+            with open("contact_tracing.txt", "r") as file:
+                matching_entries = [line.strip() for line in file if search_term in line.lower()]
+
+            if matching_entries:
+                self.display_results(matching_entries)
+            else:
+                print("No matching entries found.")
+        else:
+            print("Please enter a search term.")
+
+    def display_results(self, entries):
+        result_window = tk.Toplevel(self.root)
+        result_window.title("Search Results")
+
+        result_label = tk.Label(result_window, text="Search Results:")
+        result_label.pack()
+
+        result_text = tk.Text(result_window, height=10, width=50)
+        result_text.pack()
+
+        for entry in entries:
+            result_text.insert(tk.END, entry + "\n")
+
