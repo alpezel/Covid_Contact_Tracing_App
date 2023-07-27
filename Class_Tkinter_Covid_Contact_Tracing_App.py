be5a2b2ebd184f3f69f3910c9381386b374e8a24
alpezel <allistairevillaruel@gmail.com>
2023-07-27 12:53:05 +0800

    add validation on all inputs, add save file that makes a txt file upon submitting and making the submit button works and then test run

--- Class_Tkinter_Covid_Contact_Tracing_App.py
+++ Class_Tkinter_Covid_Contact_Tracing_App.py
@@ -59,15 +59,47 @@ class CovidContactTracingApp:
         self.disclaimer_check = tk.Checkbutton(frame, text="I Agree to the Disclaimer", variable=self.disclaimer_check_bool)
         self.disclaimer_check.pack()
 
-        self.submit_buttom = tk.Button(frame,text="Submit")
+        self.submit_buttom = tk.Button(frame,text="Submit", command=self.save_file)
         self.submit_buttom.pack()
    
 
     def validate_userinput(self):
         name = self.name_entry.get().strip()
-        if not all((name)):
+        email = self.email_entry.get().strip()
+        address = self.address_entry.get().strip()
+        number = self.number_entry.get().strip()
+        
+        if not all((name, email, address, number)):
             print("Please Fill")
             return False
+        if not self.sex.get():
+            print("Please select in the choices")
+            return False
+        if not self.disclaimer_check_bool.get():
+            print("Please Agree to the Disclaimer")
+            return False
         return True
+    
+    def save_file(self):
+        if not self.validate_userinput():
+            messagebox.showwarning("Invalid Input")
+        name = self.name_entry.get().strip()
+        email = self.email_entry.get().strip()
+        address = self.address_entry.get().strip()
+        number = self.number_entry.get().strip()
+        sex = self.sex.get()
+
+        with open("contact_tracing.txt","a") as file:
+            file.write(f"Name:{name}\n, Email:{email}\n, Address:{address}\n, Contact Number:{number}\n, Sex:{sex}\n")
+        
+        self.name_entry.delete(0,tk.END)
+        self.email_entry.delete(0,tk.END)
+        self.address_entry.delete(0,tk.END)
+        self.number_entry.delete(0,tk.END)
+        self.disclaimer_check.deselect()
+        self.sex.set("Prefer not to say")
+        print("Data Saved Successfully!")
+        messagebox.showinfo("Entry Saved","Data Saved Successfully!")
+
       
     
